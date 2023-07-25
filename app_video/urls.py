2ce2fb95257548fe0e3732d8e3df490bd9cb733b
MDW <mdw746411525@163.com>
2023-07-25 23:23:36 +0800

    add base

--- app_video/urls.py
+++ app_video/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.accordion, name='accordion')
+    path(r'/accordion', views.accordion, name='accordion')
 ]
